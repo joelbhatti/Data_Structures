dbef04e21cdb6b29920ae0550c46bfaf64fc3424
Joel Chris <cjoel5287@gmail.com>
2023-02-05 19:12:39 +0500

    Linear Searching

---
+++ linearsearching.js
@@ -0,0 +1,11 @@
+let abc = [20, 25, 8, 88, 64, 75]
+let item = 75;
+let index = undefined
+
+for (i=0; i<=length-1; i++){
+    if (abc[i]===item)
+    {
+        index = i;
+    }
+}
+console.warn(index);
