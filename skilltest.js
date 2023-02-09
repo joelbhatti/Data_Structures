2c9cbf9771c67823189b59f906258c93c1577843
Joel Chris <cjoel5287@gmail.com>
2023-02-09 11:46:11 +0500

    Added code

--- skilltest.js
+++ skilltest.js
@@ -1,5 +1,7 @@
-const jcb = {
-   name: "Joel",
-   age: 27
-}
-console.log(Object.entries(jcb))
+// const jcb = {
+//    name: "Joel",
+//    age: 27
+// }
+// console.log(Object.entries(jcb))
+let text = "This is our \"First Experience\" in this office";
+console.log(text)
