43fcd618b48574e947e3d96c4e49d5b80fcd3a93
Joel Chris <cjoel5287@gmail.com>
2023-02-16 15:29:40 +0500

    Commit to fetch

---
+++ newgitflow.js
@@ -0,0 +1,5 @@
+let x = 5;
+let y = 7;
+
+const z = x + y;
+console.log(z);
--- skilltest.js
+++ skilltest.js
@@ -3,5 +3,21 @@
 //    age: 27
 // }
 // console.log(Object.entries(jcb))
-let text = "This is our \"First Experience\" in this office";
-console.log(text)
+// let text = "This is our \"First Experience\" in this office";
+// console.log(text)
+
+function squareSum() {
+    let numbers = [1, 5, 7, 10]
+    for (i = 0; i < numbers.length; i++) {
+        let pri = numbers[i] * numbers[i];
+        console.log(pri)
+        let a ;
+        for (i = 0; i < numbers.length; i++) {
+
+            a = sum[i]
+            console.log(a)
+        }
+    }
+}
+
+squareSum();
